1d288e3f50fb8b34e1472173fa50dbab15641691
Giuseppe Naponiello <beppenapo@arc-teamcom>
2019-10-26 12:01:12 +0200

    building index carousel

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  img = ['f001.jpg', 'f002.jpg', 'f003.jpg', 'f004.jpg', 'f005.jpg', 'f006.jpg', 'f007.jpg', 'f008.jpg', 'f009.jpg', 'f010.jpg', 'f011.jpg', 'f012.jpg', 'f013.jpg', 'f014.jpg', 'f015.jpg', 'f016.jpg', 'f017.jpg', 'f018.jpg', 'f019.jpg', 'f020.jpg', 'f021.jpg', 'f022.jpg', 'f023.jpg', 'f024.jpg', 'f025.jpg', 'f026.jpg', 'f027.jpg', 'f028.jpg', 'f029.jpg', 'f030.jpg', 'f031.jpg'];
 
   constructor() { }
 
